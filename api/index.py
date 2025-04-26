18bb60b19dea4ce7421bd6e24e87349968d0bfab
iGodShoT <comar1234567890@gmail.com>
2025-04-26 18:44:50 +0300

    dkjblkdjndkj

--- api/index.py
+++ api/index.py
@@ -1,10 +1,17 @@
-import os
+import osy
 import pip
 pip.main(['install', 'pytelegrambotapi'])
 import telebot
 import time
+from flask import Flask
 
 bot = telebot.TeleBot('8005837758:AAEY0wZDeto4tchPqWnJ3DssgxXpDrrAuJk')
+
+app = Flask('')
+
+@app.route('/')
+def home():
+  return "I'm alive"
 
 @bot.message_handler(content_types=['text'])
 def get_text_message(message):
