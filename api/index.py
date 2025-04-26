158c92e0a111276b1dcab3783b4aae7078a873e7
iGodShoT <comar1234567890@gmail.com>
2025-04-26 18:58:55 +0300

    kn;dkjnd;jkd

--- api/index.py
+++ api/index.py
@@ -1,4 +1,4 @@
-import osy
+import os
 import pip
 pip.main(['install', 'pytelegrambotapi'])
 import telebot
@@ -12,10 +12,3 @@ app = Flask('')
 @app.route('/')
 def home():
   return "I'm alive"
-
-@bot.message_handler(content_types=['text'])
-def get_text_message(message):
-  bot.send_message(message.from_user.id,message.text)
-# echo-функция, которая отвечает на любое текстовое сообщение таким же текстом   
-
-bot.polling(non_stop=True, interval=0) #запуск бота
